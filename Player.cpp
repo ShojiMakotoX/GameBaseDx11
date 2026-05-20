3df0b81719d13771b5a2dc7d75dcf8bcd6e0a39f
ShojiMakotoX <250039@jc-21.jp>
2026-05-20 11:59:01 +0900

    向きなど整えた

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.h"
 #include "Engine\\Model.h"
+#include "Engine\\Input.h"
 
 Player::Player(GameObject* parent)
 //親のポインタ、オブジェクトの名前
@@ -11,10 +12,20 @@ void Player::Initialize()
 {
 	hModel_ = Model::Load("StarShip.fbx");
 	assert(hModel_ >= 0);
+	tr_.position_ = { 0.0f,-4.0f,10.0f };
+	tr_.rotate_ = { 0.0f,0.0f,0.0f };
 }
 
 void Player::Update()
 {
+	if (Input::IsKey(DIK_LEFT) || Input::IsKey(DIK_A))
+	{
+		tr_.position_.x = tr_.position_.x -= 0.5f;
+	}
+	if (Input::IsKey(DIK_RIGHT) || Input::IsKey(DIK_D))
+	{
+		tr_.position_.x = tr_.position_.x += 0.5f;
+	}
 }
 
 void Player::Draw()
